1a0bd09b4b232b360e0fbd322ec028179388a4be
brandonhach <specialcambo@gmail.com>
2024-04-05 22:46:59 -0400

    nav conditionals and show conditionals based on user auth

--- app.js
+++ app.js
@@ -18,43 +18,48 @@ let host = 'localhost';
 app.set('view engine', 'ejs');
 
 //connect to database
-mongoose.connect('mongodb://localhost:27017/demos', 
-                {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-.then(()=>{
-    //start app
-    app.listen(port, host, ()=>{
-        console.log('Server is running on port', port);
-    });
-})
-.catch(err=>console.log(err.message));
+mongoose
+	.connect('mongodb://localhost:27017/demos', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		//start app
+		app.listen(port, host, () => {
+			console.log('Server is running on port', port);
+		});
+	})
+	.catch((err) => console.log(err.message));
 
 //mount middlware
 app.use(
-    session({
-        secret: "ajfeirf90aeu9eroejfoefj",
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({mongoUrl: 'mongodb://localhost:27017/demos'}),
-        cookie: {maxAge: 60*60*1000}
-        })
+	session({
+		secret: 'ajfeirf90aeu9eroejfoefj',
+		resave: false,
+		saveUninitialized: false,
+		store: new MongoStore({ mongoUrl: 'mongodb://localhost:27017/demos' }),
+		cookie: { maxAge: 60 * 60 * 1000 },
+	})
 );
 app.use(flash());
 
 app.use((req, res, next) => {
-    //console.log(req.session);
-    res.locals.errorMessages = req.flash('error');
-    res.locals.successMessages = req.flash('success');
-    next();
+	//console.log(req.session);
+	res.locals.user = req.session.user || null;
+	res.locals.errorMessages = req.flash('error');
+	res.locals.successMessages = req.flash('success');
+	next();
 });
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 
 //set up routes
-app.get('/', (req, res)=>{
-    res.render('index');
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
 app.use('/stories', storyRoutes);
@@ -62,19 +67,18 @@ app.use('/stories', storyRoutes);
 app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
-    let err = new Error('The server cannot locate ' + req.url);
-    err.status = 404;
-    next(err);
-
+	let err = new Error('The server cannot locate ' + req.url);
+	err.status = 404;
+	next(err);
 });
 
-app.use((err, req, res, next)=>{
-    console.log(err.stack);
-    if(!err.status) {
-        err.status = 500;
-        err.message = ("Internal Server Error");
-    }
+app.use((err, req, res, next) => {
+	console.log(err.stack);
+	if (!err.status) {
+		err.status = 500;
+		err.message = 'Internal Server Error';
+	}
 
-    res.status(err.status);
-    res.render('error', {error: err});
+	res.status(err.status);
+	res.render('error', { error: err });
 });

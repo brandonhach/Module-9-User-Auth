05029856b6301c5e76eca3d542943a9aa15a1175
brandonhach <specialcambo@gmail.com>
2024-04-05 22:53:27 -0400

    validator

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,13 +21,6 @@ exports.isLoggedIn = (req, res, next) => {
 exports.isAuthor = (req, res, next) => {
 	let id = req.params.id;
 
-	// check error
-	if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-		let err = new Error('Invalid story id');
-		err.status = 400;
-		return next(err);
-	}
-
 	Story.findById(id)
 		.then((story) => {
 			if (story) {
